1967eba38709db463fe8932a311dff62216c128d
Charlie Safari <safarination333@gmail.com>
2026-05-07 17:53:03 -0700

    initial commit

---
+++ app/rand/page.tsx
@@ -0,0 +1,9 @@
+export default function Rand() {
+  return (
+    <main className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-green-500 text-6xl md:text-8xl font-mono tracking-widest">
+        sup foo
+      </h1>
+    </main>
+  );
+}
